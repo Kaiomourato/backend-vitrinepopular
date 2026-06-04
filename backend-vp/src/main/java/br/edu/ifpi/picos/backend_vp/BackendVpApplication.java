4103e1dfc003b7141d9fba588ca52a9b5fd6cca4
Kaio Mourato <kaiomouratodm@hotmail.com>
2026-06-04 15:06:21 -0300

    Spring Initializr

---
+++ backend-vp/src/main/java/br/edu/ifpi/picos/backend_vp/BackendVpApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.picos.backend_vp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVpApplication.class, args);
+	}
+
+}
---
+++ backend-vp/src/test/java/br/edu/ifpi/picos/backend_vp/BackendVpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.picos.backend_vp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
